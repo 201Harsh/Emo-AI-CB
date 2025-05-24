1c693c47ca5f3d485e33bf5d11e25d995b31051d
Harsh <gamerpandeyharsh@gmail.com>
2025-05-24 11:34:10 +0530

    otp UI Page

--- Frontend/src/Routes/Route.jsx
+++ Frontend/src/Routes/Route.jsx
@@ -10,6 +10,7 @@ import Contact from "../Pages/Contact";
 import About from "../Pages/About";
 import Pricing from "../Pages/Pricing";
 import AuthPrompt from "../Pages/AuthPrompt";
+import OTPVerification from "../Pages/OtpVerification";
 
 const Router = () => {
   return (
@@ -18,6 +19,7 @@ const Router = () => {
         <Route path="/" element={<Start />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/otp" element={<OTPVerification />} />
         <Route path="/authprompt" element={<AuthPrompt />} />
         <Route path="/home" element={<Home />} />
         <Route path="/features" element={<Features />} />
