a3f4b77b12afe9a95b5cbc4c33a010ea2bb6ef64
Harsh <gamerpandeyharsh@gmail.com>
2025-08-14 23:25:46 +0530

    Production Level Ready

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,6 @@ const http = require("http");
 
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
-const host = process.env.HOST || "0.0.0.0";
 
-server.listen(port, host, () => {
+server.listen(port,  () => {
 });
